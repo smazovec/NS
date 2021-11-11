cb583fae8c22c84f7f194223da5b795709a4e91c
Lezenford <alex.v.plekhanov@gmail.com>
2021-11-11 19:46:17 +0300

    init commit

---
+++ src/main/java/com/lezenford/networkstoragetemplate/NetworkStorageTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.lezenford.networkstoragetemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkStorageTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkStorageTemplateApplication.class, args);
+	}
+
+}
