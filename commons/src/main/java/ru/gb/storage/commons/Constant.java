a70bae8de6e703e97d9a6619bb0d61b67f2ab145
Sergey Mazovets <smazovets@gmail.com>
2022-06-09 23:11:41 +0700

    preparation of the project structure

--- commons/src/main/java/ru/gb/storage/commons/Constant.java
+++ commons/src/main/java/ru/gb/storage/commons/Constant.java
@@ -1,4 +1,5 @@
 package ru.gb.storage.commons;
 
 public class Constant {
+
 }
--- server/src/main/java/ru/gb/storage/server/Server.java
+++ server/src/main/java/ru/gb/storage/server/Server.java
@@ -1,7 +1,8 @@
 package ru.gb.storage.server;
 
 public class Server {
-    public static void main(String[] args) {
-        System.out.println("Server started");
-    }
+
+  public static void main(String[] args) {
+    System.out.println("Server started");
+  }
 }
